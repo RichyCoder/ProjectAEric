d84984fa9cc9b90f98307e611728b866c92335b3
Richard Simpson <coolcatalex@gmail.com>
2012-04-09 19:46:32 -0500

    Adding basic site structure. Testing phone gap.

---
+++ scripts/_vti_cnf/cordova-1.5.0.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|CORP\\richard.simpson
+vti_modifiedby:SR|CORP\\richard.simpson
+vti_timelastmodified:TR|10 Apr 2012 00:45:07 -0000
+vti_timecreated:TR|10 Apr 2012 00:45:07 -0000
+vti_cacheddtm:TX|10 Apr 2012 00:45:07 -0000
+vti_filesize:IR|153359
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
